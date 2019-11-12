176a2e45e0cd57e9f95d1f8f44450f307dc7b616
MichaiLeonidou <44082204+MichaiLeonidou@users.noreply.github.com>
2019-11-12 21:02:12 +0000

    Updates

---
+++ precache-manifest.96c84d68fc9f5529c1f331cfdec0c4fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3faf812550f1eb1df0415a87f0cf0ab",
+    "url": "/portofolio/index.html"
+  },
+  {
+    "revision": "11e0e25845261e24f8c8",
+    "url": "/portofolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "df2b5f8be0e045aad714",
+    "url": "/portofolio/static/css/main.2001834d.chunk.css"
+  },
+  {
+    "revision": "11e0e25845261e24f8c8",
+    "url": "/portofolio/static/js/2.ede030d5.chunk.js"
+  },
+  {
+    "revision": "df2b5f8be0e045aad714",
+    "url": "/portofolio/static/js/main.94bf50db.chunk.js"
+  },
+  {
+    "revision": "651cc391e0f68fce8183",
+    "url": "/portofolio/static/js/runtime-main.3ebaa354.js"
+  }
+]);
