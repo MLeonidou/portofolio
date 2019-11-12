16b685d01d2b04eca122127550068aafd42480ba
MichaiLeonidou <44082204+MichaiLeonidou@users.noreply.github.com>
2019-11-12 22:21:13 +0000

    Updates

--- precache-manifest.17a330e656ee92a6743aa1c43c28795e.js
+++ precache-manifest.17a330e656ee92a6743aa1c43c28795e.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ded696b5cf04f8df73b9289b4cfba62a",
+    "revision": "866ac5e93b30beaf6444e9c8227daa53",
     "url": "/portofolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portofolio/static/css/2.559fd73f.chunk.css"
   },
   {
-    "revision": "65e0d76bd3d420a47680",
+    "revision": "1c43b77f98a29a19ab79",
     "url": "/portofolio/static/css/main.2001834d.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/portofolio/static/js/2.ede030d5.chunk.js"
   },
   {
-    "revision": "65e0d76bd3d420a47680",
-    "url": "/portofolio/static/js/main.20e0cb69.chunk.js"
+    "revision": "1c43b77f98a29a19ab79",
+    "url": "/portofolio/static/js/main.d4034c98.chunk.js"
   },
   {
     "revision": "651cc391e0f68fce8183",
